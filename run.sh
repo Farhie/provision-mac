cb0c80b354ed6564f7c15be8ccb0df2d363defaa
Adam Fahie <adam.fahie@gmail.com>
2017-11-28 09:05:04 -0800

    Adam - Remove fish, add google drive, update to correct sudo password name

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@ set -e
 
 PASSWORD=$1
 
-ansible-playbook playbook.yml --extra-vars "ansible_sudo_pass=$PASSWORD" -i HOSTS
+ansible-playbook playbook.yml --extra-vars "ansible_become_pass=$PASSWORD" -i HOSTS
 
 exit 0
